3b0e3e1c7ae5c1ebb7faed3de7327dc2bcebae47
Adeesha <adeeshadilmira2@gmail.com>
2026-07-04 10:54:51 +0530

    Build first machine learning model with decision tree

--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 data = pd.read_csv("students.csv")
 
@@ -23,11 +24,33 @@ model = DecisionTreeClassifier(random_state=42)
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 
+accuracy = accuracy_score(y_test, predictions)
+
 print("Actual:")
 print(y_test)
 
 print("\nPredicted:")
 print(predictions)
 
+print(f"\nAccuracy: {accuracy * 100:.2f}%")
+
 print("Training Data:", len(X_train))
 print("Testing Data:", len(X_test))
+
+print("="*40)
+print("New Student Prediction")
+print("="*40)
+
+attendance = int(input("Enter your attendance (%): "))
+assignment = int(input("Enter your assignment marks: "))
+quiz = int(input("Enter quiz marks: "))
+
+new_student = pd.DataFrame({
+    "Attendance": [attendance],
+    "Assignment": [assignment],
+    "Quiz": [quiz]
+})
+
+prediction = model.predict(new_student)
+
+print("\nPrediction:", prediction[0])
