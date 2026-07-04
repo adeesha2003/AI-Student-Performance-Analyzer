1e6753d341309c13b24e586610cee2ded27caff9
Adeesha <adeeshadilmira2@gmail.com>
2026-07-04 07:35:54 +0530

    Add functions and input validation

--- main.py
+++ main.py
@@ -2,32 +2,44 @@ print("="*40)
 print("Student Performance Analyzer")
 print("="*40)
 
+# Function to calculate grade
+def calculate_grade(marks):
+    if marks >= 75:
+        return "A"
+    elif marks >= 65:
+        return "B"
+    elif marks >= 50:
+        return "C"
+    else:
+        return "F"
+    
+#Function to calculate pass/fail status
+def calculate_status(grade):
+    if grade == "F":
+        return "Fail"
+    else:
+        return "Pass"
+
 # Get student name
-student_name = input("Enter your name: ")
+name = input("Enter your name: ")
 
 # Get student marks
-student_marks = int(input("Enter your marks: "))
+while True:
+    marks = int(input("Enter your marks: "))
+    if 0 <= marks <= 100:
+        break
+    else:
+        print("Invalid marks. Please enter a value between 0 and 100.")
 
-# Calculate grade
-if student_marks >= 75:
-    grade = "A"
-elif student_marks >= 65:
-    grade = "B"
-elif student_marks >= 50:
-    grade = "C"
-else:
-    grade = "F"
-
-# Calculate status
-if grade == "F":
-    status = "Fail"
-else:
-    status = "Pass"
+#Calculate grade
+grade = calculate_grade(marks)
+status = calculate_status(grade)
 
 # Display results
 print("="*40)
-print("Student:", student_name)
-print("Marks:", student_marks)
+print("Student Report")
+print("="*40)
+print("Student:", name)
+print("Marks:", marks)
 print("Grade:", grade)
 print("Status:", status)
-print("="*40)
