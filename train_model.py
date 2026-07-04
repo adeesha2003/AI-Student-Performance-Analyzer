7abe4a25344ed7c9abe3ba6bff97dd0028ad1af5
Adeesha <adeeshadilmira2@gmail.com>
2026-07-04 10:17:12 +0530

    Build first ML model with student dataset

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 data = pd.read_csv("students.csv")
 
@@ -10,3 +11,23 @@ y = data["Status"]
 
 print(X.head())
 print(y.head())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+print("Actual:")
+print(y_test)
+
+print("\nPredicted:")
+print(predictions)
+
+print("Training Data:", len(X_train))
+print("Testing Data:", len(X_test))
