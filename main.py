2f6b8ae6c5a286c4eeed527ec74e45dba2a119f1
Adeesha <adeeshadilmira2@gmail.com>
2026-07-04 08:42:45 +0530

    Add multiple student support and CSV export

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 print("="*40)
 print("Student Performance Analyzer")
 print("="*40)
@@ -19,27 +21,48 @@ def calculate_status(grade):
         return "Fail"
     else:
         return "Pass"
+    
+# Create CSV file if it doesn't exist
+if not os.path.exists("students.csv"):
+    file = open("students.csv", "w")
+    file.write("Name,Marks,Grade,Status\n")
+    file.close()
 
-# Get student name
-name = input("Enter your name: ")
-
-# Get student marks
 while True:
-    marks = int(input("Enter your marks: "))
-    if 0 <= marks <= 100:
+    # Get student name
+    name = input("Enter your name: ")
+
+    # Get student marks
+    while True:
+        marks = int(input("Enter your marks: "))
+        if 0 <= marks <= 100:
+            break
+        else:
+            print("Invalid marks. Please enter a value between 0 and 100.")
+
+    #Calculate grade and status
+    grade = calculate_grade(marks)
+    status = calculate_status(grade)
+
+    # Display results
+    print("="*40)
+    print("Student Report")
+    print("="*40)
+    print("Student:", name)
+    print("Marks:", marks)
+    print("Grade:", grade)
+    print("Status:", status)
+
+    file = open("students.csv", "a")
+    file.write(f"{name},{marks},{grade},{status}\n")
+    file.close()
+
+    # Ask user to continue
+    choice = input("\nDo you want to add another student? (y/n): ")
+
+    if choice.lower() == "n":
+        print("\nThank you for using the Student Performance Analyzer!")
         break
-    else:
-        print("Invalid marks. Please enter a value between 0 and 100.")
 
-#Calculate grade
-grade = calculate_grade(marks)
-status = calculate_status(grade)
 
-# Display results
-print("="*40)
-print("Student Report")
-print("="*40)
-print("Student:", name)
-print("Marks:", marks)
-print("Grade:", grade)
-print("Status:", status)
+
